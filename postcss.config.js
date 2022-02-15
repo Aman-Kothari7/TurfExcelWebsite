64cb42292c36b389b066ddaab61a05bc4d44357e
prem-jethwa <premjethwa788@gmail.com>
2022-02-15 15:15:21 +0530

    remove commends

--- postcss.config.js
+++ postcss.config.js
@@ -4,14 +4,3 @@ module.exports = {
     autoprefixer: {},
   },
 };
-
-// {
-//   "hosting": {
-//     "public": "out",
-//     "ignore": ["firebase.json", "**/.*", "**/node_modules/**"],
-//     "trailingSlash": false,
-//     "SERVICE_ID": "service_gmw24xm",
-//     "TEMPLATE_ID": "template_tcf95x6",
-//     "USER_ID": "user_1gRhWAL9UKeLrRu4fD6dU"
-//   }
-// }
